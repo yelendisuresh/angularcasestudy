b52db12f74eeb09d5abb9ae36929051d5ee7f48e
yelendisuresh <yelendisuresh@gmail.com>
2021-12-06 11:50:26 +0530

    refactor of code

--- src/app/route6/route6.component.ts
+++ src/app/route6/route6.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-route6',
@@ -42,7 +42,7 @@ export class Route6Component implements OnInit, AfterViewInit, OnDestroy {
     for (let i = 1; i <= count; ++i) {
       var div = document.createElement('div');
       div.innerHTML = `<button class="buttonClass" type="button"> ${this.noOfdivs}</button>`;
-      div.style.cssText = `height:200px;width:200px;background-color:#8bc34a;border-radius:4px;display: flex;align-items: center;justify-content: center;float:left;margin-left: 10px;margin-bottom: 10px;`;
+      div.style.cssText = `background-color:#8bc34a;border-radius:4px;display: flex;align-items: center;justify-content: center;float:left; height:200px;width:200px;margin-left: 10px;margin-bottom: 10px;`;
       this.noOfdivs++;
       button?.appendChild(div);
     }
@@ -52,6 +52,6 @@ export class Route6Component implements OnInit, AfterViewInit, OnDestroy {
     document?.querySelector('#buttons')?.removeEventListener("click", () => {
       console.log("onclick removed");
     });
-    window.removeEventListener('scroll',()=>{ console.log("scroll removed")});
+    window.removeEventListener('scroll', () => { console.log("scroll removed") });
   }
 }
