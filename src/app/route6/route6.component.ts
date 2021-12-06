ccf22c48e2476cf4ae5a364dd14a118e1c4d5846
yelendisuresh <yelendisuresh@gmail.com>
2021-12-06 11:43:56 +0530

    initaial

--- src/app/route6/route6.component.ts
+++ src/app/route6/route6.component.ts
@@ -1,54 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-route6',
   templateUrl: './route6.component.html',
   styleUrls: ['./route6.component.scss']
 })
-export class Route6Component implements OnInit {
-
+export class Route6Component implements OnInit, AfterViewInit, OnDestroy {
+  noOfdivs = 1;
   constructor() { }
 
   ngOnInit(): void {
-    this.createDynamicDivs(20);   
+    this.createDynamicDivs(10);
   }
 
   ngAfterViewInit() {
-    document?.querySelector('#buttons')?.addEventListener('click', (event:any) => {
-      if(event.target.className === 'buttonClass'){
-      console.log(event?.target?.getInnerHTML() + "clicked");
+    document?.querySelector('#buttons')?.addEventListener('click', (event: any) => {
+      if (event.target.className === 'buttonClass') {
+        console.log(event?.target?.getInnerHTML() + "clicked");
       }
     });
-    let oldValue = 0;
-    window.addEventListener('scroll', (event:any) => {
-      // console.log(event);
-       // Get the new Value
-   let  newValue = window.pageYOffset;
 
-    //Subtract the two and conclude
-    if(oldValue - newValue < 0){
-        console.log("Up");
-    } else if(oldValue - newValue > 0){
-        console.log("Down");
-    }
-
-    // Update the old value
-    oldValue = newValue;
-
+    let oldValue = 0, newValue = 0;
+    window.addEventListener('scroll', (e: any) => {
+      newValue = window.pageYOffset;
+      if (oldValue < newValue) {
+        console.log("down", e.currentTarget.pageYOffset);
+        if (e.currentTarget.pageYOffset > 150 && this.noOfdivs < 60) {
+          this.createDynamicDivs(20);
+          console.log(" createDynamicDivs", this.noOfdivs)
+        }
+      } else if (oldValue > newValue) {
+        console.log("up");
+      }
+      oldValue = newValue;
     });
 
   }
 
-  private createDynamicDivs(count:number){
-    for (let i = 1;i < count;++i)
-    {
-      const button=document.querySelector('#buttons')
+  private createDynamicDivs(count: number) {
+    const button = document.querySelector('#buttons');
+    for (let i = 1; i <= count; ++i) {
       var div = document.createElement('div');
-      div.innerHTML = `<button class="buttonClass" type="button"> ${i}</button>`; 
+      div.innerHTML = `<button class="buttonClass" type="button"> ${this.noOfdivs}</button>`;
       div.style.cssText = `height:200px;width:200px;background-color:#8bc34a;border-radius:4px;display: flex;align-items: center;justify-content: center;float:left;margin-left: 10px;margin-bottom: 10px;`;
-
-     button?.appendChild(div);
+      this.noOfdivs++;
+      button?.appendChild(div);
     }
   }
 
+  ngOnDestroy() {
+    document?.querySelector('#buttons')?.removeEventListener("click", () => {
+      console.log("onclick removed");
+    });
+    window.removeEventListener('scroll',()=>{ console.log("scroll removed")});
+  }
 }
